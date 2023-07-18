0297492e047f369763981d7eabace7088614ddc0
choichanggeun <choichanggeoun@gmail.com>
2023-07-18 20:58:07 +0900

    ddddd

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 4000;
 
-//dotenv.config(); 
+//dotenv.config();
 
 const morgan = require("morgan");
 
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
-const indexRouter = require('./router/index.js');
+const indexRouter = require("./router/index.js");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(morgan("dev"))
-app.use('/api', indexRouter);
-
+app.use(morgan("dev"));
+app.use("/api", indexRouter);
 
 app.listen(port, () => {
-  console.log(port, '번 포트로 서버가 실행되었습니다.')
+  console.log(port, "번 포트로 서버가 실행되었습니다.");
 });
+//ddd
