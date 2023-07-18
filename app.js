2b9ebcab5243aca51b11f064afa79a009188c94a
parkchaewon <xkdldjaos20@gmail.com>
2023-07-19 02:53:23 +0900

    로그인,회원가입 구현

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
+require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
 const app = express();
 const port = 4000;
+const { SECRET_KEY } = process.env;
+const dotenv = require('dotenv');
+dotenv.config();
 
-//dotenv.config(); 
-
-const morgan = require("morgan");
+const morgan = require('morgan');
 
 const cookieParser = require('cookie-parser');
 
 const indexRouter = require('./router/index.js');
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(
+  session({
+    secret: SECRET_KEY,
+    resave: false,
+    rolling: true,
+    saveUninitialized: false,
+    store: new MemoryStore({ checkPeriod: 1000 * 60 * 60 }),
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(morgan("dev"))
+app.use(morgan('dev'));
 app.use('/api', indexRouter);
 
-
 app.listen(port, () => {
-  console.log(port, '번 포트로 서버가 실행되었습니다.')
+  console.log(port, '번 포트로 서버가 실행되었습니다.');
 });
