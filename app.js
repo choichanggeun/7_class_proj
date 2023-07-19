c5de011439706fd17e5be67624bbc656f488ae69
choichanggeun <choichanggeoun@gmail.com>
2023-07-19 14:00:47 +0900

    dddd

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 4000;
 
 //dotenv.config();
-require("dotenv").config();
+require('dotenv').config();
 
 const rdsUsername = process.env.RDS_USERNAME;
 const rdsPassword = process.env.RDS_PASSWORD;
 const rdsHost = process.env.RDS_HOST;
 const rdsPort = process.env.RDS_PORT;
 
-const morgan = require("morgan");
+const morgan = require('morgan');
 
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 
-const indexRouter = require("./router/index.js");
+const indexRouter = require('./router/index.js');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(morgan("dev"));
-app.use("/api", indexRouter);
+app.use(morgan('dev'));
+app.use('/api', indexRouter);
 
 app.listen(port, () => {
-  console.log(port, "번 포트로 서버가 실행되었습니다.");
+  console.log(port, '번 포트로 서버가 실행되었습니다.');
 });
-//ddd
-//ddddd
