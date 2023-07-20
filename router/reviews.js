984051baf24d0ba3e55c1a73f503428037c9248d
rakulow2 <133615911+rakulow2@users.noreply.github.com>
2023-07-21 02:01:09 +0900

    Update reviews.js

--- router/reviews.js
+++ router/reviews.js
@@ -17,14 +17,21 @@ router.post(':reservationId/createdReviews', auth, async (req, res) => {
     return res.status(400).json({ erorMessage: '예약이 존재하지 않습니다.' });
   }
 
-  const { title, content, } = req.body;
+  const { title, content } = req.body;
   const createdReview = await Reviews.create({
     ReservationId: reservationId,
     title,
-    content,
+    content
   });
   return res.status(200).json({ data: createdReview });
 });
+
+   /*  const { title, content, rating } = req.body;
+    const createdReview = await Reviews.create({
+        ReservationId: reservationId,
+        title,
+        content,
+        rating  })*/
 
 //전체 리뷰 불러오기
 router.get('/allReview', auth, async (req, res) => {
@@ -59,25 +66,24 @@ router.get('/:reviewId', auth, async (req, res) => {
 });
 
 //리뷰 정보 수정
-router.put('/:reservationId', auth, async (req, res) => {
-const { reservationId } = req.params;
-const { startDate, endDate, petSitter } = req.body;
+router.put('/:reveiwID', auth, async (req, res) => {
+const { reviewId } = req.params;
+const { title, content } = req.body;
 
 try {
-const reservation = await Reservations.findOne({ where: { reservationId } });
+const review = await review.findOne({ where: { reviewId } });
 
-if (!reservation) {
-res.status(404).json({ message: '예약 정보가 존재하지 않습니다.' });
+if (!review) {
+res.status(404).json({ message: '리뷰 정보가 존재하지 않습니다.' });
 return;
 }
 
-await reservation.update({
-startDate,
-endDate,
-petSitter,
+await review.update({
+content,
+title
 });
 
-res.status(200).json({ message: '예약 변경이 완료 되었습니다.' });
+res.status(200).json({ message: '리뷰 변경이 완료 되었습니다.' });
 } catch (error) {
 console.log(error);
 res.status(500).json({ message: error });
@@ -85,20 +91,20 @@ res.status(500).json({ message: error });
 });
 
 // 리뷰 삭제
-router.delete('/:reservationId', auth, async (req, res) => {
-const { reservationId } = req.params;
+router.delete('/:reviewId', auth, async (req, res) => {
+const { reviewId } = req.params;
 
 try {
-const reservation = await Reservations.findOne({ where: { reservationId } });
+const review = await review.findOne({ where: { reviewId } });
 
-if (!reservation) {
-res.status(404).json({ message: '예약 정보가 존재하지 않습니다.' });
+if (!review) {
+res.status(404).json({ message: '리뷰가 존재하지 않습니다.' });
 return;
 }
 
-await reservation.destroy();
+await review.destroy();
 
-res.status(200).json({ message: '예약 삭제가 완료 되었습니다.' });
+res.status(200).json({ message: '리뷰 삭제가 완료 되었습니다.' });
 } catch (error) {
 console.log(error);
 res.status(500).json({ message: error });
@@ -106,4 +112,3 @@ res.status(500).json({ message: error });
 });
 
 module.exports = router;
-
